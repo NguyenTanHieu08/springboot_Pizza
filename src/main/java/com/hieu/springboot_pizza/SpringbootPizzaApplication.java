b105681db8190af19d1961976544e5dd65415bce
Nguyen Tan Hieu <hieuntts2103013@fpt.edu.vn>
2025-07-10 15:54:06 +0700

    add all files

---
+++ src/main/java/com/hieu/springboot_pizza/SpringbootPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.hieu.springboot_pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPizzaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hieu/springboot_pizza/SpringbootPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hieu.springboot_pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
